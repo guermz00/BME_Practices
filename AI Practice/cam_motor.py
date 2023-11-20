f1c8e161d89ebdd9de22de72e320b6e5dc706723
guermz00 <guillermz00@gmail.com>
2023-11-20 01:31:01 -0600

    Added motor + AI

---
+++ AI Practice/cam_motor.py
@@ -0,0 +1,25 @@
+import serial
+import time
+from picamera import PiCamera
+
+# Initialize camera
+camera = PiCamera()
+camera.resolution = (1024, 768)
+
+# Initialize serial communication with Arduino
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.flush()
+
+def capture_image():
+    camera.capture('image.jpg')
+    print("Image captured")
+
+def send_command_to_arduino(command):
+    ser.write(f"{command}\n".encode('utf-8'))
+    time.sleep(1)  # Wait for Arduino to process the command
+
+while True:
+    input("Press Enter to capture image and move motor...")
+    capture_image()
+    steps = 100  # Define the number of steps the motor should move
+    send_command_to_arduino(steps)
---
+++ AI Practice/stepper_motor_AI.ino
@@ -0,0 +1,23 @@
+#include Stepper.h
+
+const int stepsPerRevolution = 200;   change this to fit the number of steps per revolution for your motor
+
+ Pins connected to the ULN2003 driver inputs
+int motorPin1 = 8;
+int motorPin2 = 9;
+int motorPin3 = 10;
+int motorPin4 = 11;
+
+Stepper myStepper(stepsPerRevolution, motorPin1, motorPin3, motorPin2, motorPin4); 
+
+void setup() {
+  myStepper.setSpeed(60);   set the speed at 60 rpm
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()  0) {
+    int steps = Serial.parseInt();   read the number of steps to move
+    myStepper.step(steps);   move the motor
+  }
+}
